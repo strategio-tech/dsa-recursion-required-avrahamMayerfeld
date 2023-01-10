5c259729ec0aafd19c2e60541dc85c439aabd6c3
abm8 <abm8@protonmail.com>
2023-01-09 20:24:41 -0500

    added return statement and removed unnecessary code

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -10,20 +10,16 @@ public class E01 {
         if(n == 1)
         {
             fib[n] = 0;
-            return fib[n];
         }
         if(n == 2)
         {
             fib[n] = 1;
-            return fib[n];
         }
-
-        if(fib[n] != 0)
-            return fib[n];
-        else if (n > 2)
+        if(fib[n] == 0 && n > 2)
         {
             fib[n] = getNthFib(n-1) + getNthFib(n-2);
             return fib[n];
         }
+        return fib[n];
     }
 }
