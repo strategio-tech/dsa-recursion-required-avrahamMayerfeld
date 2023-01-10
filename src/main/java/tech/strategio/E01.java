8b571f5309fc89aebd16f06fdee735bc9022ddca
abm8 <abm8@protonmail.com>
2023-01-09 20:18:27 -0500

    changed a bunch of logic

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -3,17 +3,26 @@ package tech.strategio;
 import java.util.*;
 
 public class E01 {
+
+
     public int getNthFib(int n) {
         int[] fib = new int[n + 1];
-        fib[0] = 0;
-        fib[1] = 0;
-        fib[2] = 1;
+        if(n == 1)
+        {
+            fib[n] = 0;
+            return fib[n];
+        }
+        if(n == 2)
+        {
+            fib[n] = 1;
+            return fib[n];
+        }
 
         if(fib[n] != 0)
             return fib[n];
-        else
+        else if (n > 2)
         {
-            fib[n] = fib[n-1] + fib[n-2];
+            fib[n] = getNthFib[n-1] + getNthFib[n-2];
             return fib[n];
         }
     }
